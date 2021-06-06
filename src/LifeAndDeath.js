18d5b788078ad3b1a95264ff05ec075276954688
alabsi91 <58077449+alabsi91@users.noreply.github.com>
2021-06-06 19:15:18 +0300

    export selected drawing

--- src/LifeAndDeath.js
+++ src/LifeAndDeath.js
@@ -531,6 +531,7 @@ export default class GameOfLife extends Component {
       <LoadCards
         loadHandle={() => this.loadSaves(i)}
         removeSaveHandle={e => this.removeSave(e, i)}
+        exportSaveHandle={ev => this.exportSelectedData(ev, i, e.savingName)}
         img={e.drawngImg}
         name={e.savingName}
         width={e.saveSettings[0]}
@@ -710,6 +711,14 @@ export default class GameOfLife extends Component {
     const myData = localStorage.getItem('saved');
     const myblob = new Blob([myData], { type: 'application/json' });
     saveAs(myblob, 'saved-drawings', { type: 'application/json' });
+  };
+
+  exportSelectedData = (e, i, name) => {
+    e.stopPropagation();
+    const saved = JSON.parse(localStorage.getItem('saved'));
+    const toExport = JSON.stringify([saved[i]]);
+    const myblob = new Blob([toExport], { type: 'application/json' });
+    saveAs(myblob, name, { type: 'application/json' });
   };
 
   confirmAdd = () => {
@@ -1595,9 +1604,14 @@ function LoadCards(p) {
           Size : {p.width} x {p.height} Grid
         </p>
       </div>
-      <button className='deleteSave' onClick={p.removeSaveHandle}>
+      <button className='deleteSave' onClick={p.removeSaveHandle} title='Delete this from saved drawings'>
         <svg xmlns='http://www.w3.org/2000/svg' height='24px' viewBox='0 0 24 24' width='24px' fill='#D7D7D7'>
           <path d='M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z' />
+        </svg>
+      </button>
+      <button className='exportSave' onClick={p.exportSaveHandle} title='Export this to JSON file'>
+        <svg xmlns='http://www.w3.org/2000/svg' height='20px' viewBox='0 0 24 24' width='20px' fill='#D7D7D7'>
+          <path d='M23 0v20h-8v-2h6v-16h-18v16h6v2h-8v-20h22zm-12 13h-4l5-6 5 6h-4v11h-2v-11z' />{' '}
         </svg>
       </button>
     </div>
